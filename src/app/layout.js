0da9c337ab7234d5fb084e37f6bdb3ddd25530e1
ManishJoc14 <manishjoc14@gmail.com>
2024-04-17 23:31:04 +0545

    og image added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,13 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import TransitionProvider from "@/components/TransitionProvider";
-
+import ogimage from "../../public/manish.jpg" 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Manish",
   description: "The best portfolio website ever!",
+  image: ogimage,
 };
 
 export default function RootLayout({ children }) {
